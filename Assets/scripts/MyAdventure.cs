98184b1531f5c79b1894bbc33b3345392fdd638f
Tyra Afoley Okai Cudjoe <tyra20015@gmail.com>
2020-09-17 09:53:25 +0200

    Initial commit

---
+++ Assets/scripts/MyAdventure.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyAdventure : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Hello World");
+        Terminal.WriteLine("Hoi ik zit in de terminal");
+        ShowMainMenu();
+    }
+
+    void ShowMainMenu()
+    {
+        Terminal.ClearScreen();
+        Terminal.WriteLine("Welcome to Self Disaster!");
+        Terminal.WriteLine("This story can happen to each and everyone of us");
+        Terminal.WriteLine("so be aware of your surroundings after you finish this game.");
+        Terminal.WriteLine("Type 'start' to start the game.");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
